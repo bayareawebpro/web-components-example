1b3a6aec8e2ab7bb5546a470ec3ff1e5fc4c02fe
Dan Alvidrez <dan@bayareawebpro.com>
2024-01-06 02:54:57 -0800

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
-import List from "./List.js";
-import ListItem from "./ListItem.js";
-import ListForm from "./ListForm.js";
+import List from "/components/List.js";
+import ListItem from "/components/ListItem.js";
+import ListForm from "/components/ListForm.js";
 
 customElements.define('custom-list', List)
 customElements.define('custom-list-item', ListItem)
