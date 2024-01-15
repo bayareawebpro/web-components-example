d71a5b615a89f2598246adfd8724b7a700521f5f
Dan Alvidrez <dan@bayareawebpro.com>
2024-01-15 05:56:51 -0800

    Update StateBinding.js

--- directives/StateBinding.js
+++ directives/StateBinding.js
@@ -2,10 +2,6 @@ import Directive from "./Directive.js";
 import Component from "../components/Component.js";
 
 export default class StateBinding extends Directive {
-
-    parse(){
-
-    }
     execute() {
         if(this.element instanceof Component){
             this.element.setState(this.expression, this.evaluate());
